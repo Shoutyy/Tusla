3548bd13c54a59e9c75834d565c22ec64e3bf02b
Benjamin Debray <shouty91600@hotmail.fr>
2018-06-18 11:53:37 +0200

    Mise en place des modèles

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_18_092040) do
+ActiveRecord::Schema.define(version: 2018_06_18_093649) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.string "genre"
+    t.integer "price"
+    t.boolean "statut"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "content"
+    t.integer "rating"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
