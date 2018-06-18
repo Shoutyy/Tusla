fa5ace8a0e5fd60c41a67dda039944c7e40e781c
Benjamin Debray <shouty91600@hotmail.fr>
2018-06-18 17:36:30 +0200

    Home page en cours

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+
+  get 'profile', to: 'profile#show'
+
+  resources :profile, only: :show
+  resources :products do
+    resources :review, only: :create
+  end
+
+  resources :reviews, only: :destroy
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
